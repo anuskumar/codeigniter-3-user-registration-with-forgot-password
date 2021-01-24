66021a04bf02bff809a4c0c1f5dd97518e0020e5
ANU S KUMAR <50862045+anuskumar@users.noreply.github.com>
2021-01-24 12:57:17 +0530

    primary

---
+++ application/views/header.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang=""> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/user-registration-codeigniter/public/css/main.css">
+    </head>
+    <body>
+    <!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+    <![endif]-->
+    <?php
+            $flash_message = $this->session->flashdata('flash_message'); 
+            if(!empty($flash_message)){
+                $html = '<div class="bg-warning container flash-message">';
+                $html .= $flash_message; 
+                $html .= '</div>';
+                echo $html;
+                // echo "hiiiiii";
+
+            }
+        ?>
+    <div class="container">
+        <div class="row">
+        
+        
+         
+        
